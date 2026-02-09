fd6e8b1dc5f0b388a6278d6964938c84d6199c8a
Reyar <2290197845@qq.com>
2026-02-09 22:52:39 +0800

    Finished Saveload and Checkpoints

--- GameManager/GameManager.cs
+++ GameManager/GameManager.cs
@@ -1,32 +1,153 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
 
 public partial class GameManager : Node
 {
 	public static GameManager Instance;
 
 	public int CurrentCheckPointID = 0;
-	public List<int> DestroyedWalls = new();
+	public int CurrentMaxPoints = 0;
+	public Player Player;
+	public List<CheckPoint> CheckPoints = new();
+
+	public string SaveFilePath = "user://savedata.dat";
+	[Signal] public delegate void CheckPointChangedEventHandler(int checkPointID);
+
+	
+
 	public override void _Ready()
 	{
 		Instance = this;
+	}
+	public void GetAllCheckPoints()
+	{
+		CheckPoints.Clear();
+		var nodes = GetTree().GetNodesInGroup("CheckPoints");
+		foreach (var node in nodes)
+		{
+			if (node is CheckPoint checkPoint)
+			{
+				GD.Print($"GameManager: Found CheckPoint ID {checkPoint.CheckPointID}");
+				CheckPoints.Add(checkPoint);
+			}
+		}
+	}
+
+	public async void StartNewGame()
+	{
+		CurrentCheckPointID = -1;
+		CurrentMaxPoints = 0;
+		await LoadGame();
+		RespawnPlayer(CurrentCheckPointID);
+		SaveFile(CurrentCheckPointID);
+	}
+	public async void ContinueGame()
+	{
+		await LoadGame();
+		await LoadSaveFile();
+		RespawnPlayer(CurrentCheckPointID);
+		SetCheckPoint(CurrentCheckPointID);
+	}
+
+	public async Task LoadGame()
+	{
+		await Task.Delay(5);
+		GetAllCheckPoints();
+	}
+
+	public void RespawnPlayer(int checkPointID)
+	{
+		GD.Print($"GameManager: Respawning player at checkpoint {checkPointID}");
+		foreach (var checkPoint in CheckPoints)
+		{
+			if (checkPoint.CheckPointID == checkPointID)
+			{
+				Player.GlobalPosition = checkPoint.GlobalPosition;
+				return;
+			}
+		}
+		GD.PrintErr($"GameManager: Checkpoint {checkPointID} not found!");
 	}
 
 	public void SetCheckPoint(int checkPointID)
 	{
 		CurrentCheckPointID = checkPointID;
 		GD.Print($"GameManager: Checkpoint set to {CurrentCheckPointID}");
-		// TODO: Write checkpoint data to file
+		SaveFile(checkPointID);
+		EmitSignal("CheckPointChanged", checkPointID);
 	}
 
-	public void DestroyWall(int wallID)
+	public void SaveFile(int checkPointID)
 	{
-		if (!DestroyedWalls.Contains(wallID))
+		SaveData saveData = new SaveData
 		{
-			DestroyedWalls.Add(wallID);
-			GD.Print($"GameManager: Wall {wallID} destroyed.");
+			CheckPointID = checkPointID,
+			MaxPoints = CurrentMaxPoints
+		};
+
+		var file = Godot.FileAccess.Open(SaveFilePath, Godot.FileAccess.ModeFlags.Write);
+		if (file == null)
+		{
+			GD.PrintErr("GameManager: Failed to open save file for writing.");
+			return;
 		}
-		// TODO: Write destroyed walls data to file
+
+		var json = JsonSerializer.Serialize(saveData);
+		file.StoreString(json);
+		file.Close();
+		GD.Print("GameManager: Game saved successfully.");
 	}
+	public async Task LoadSaveFile()
+	{
+		var file = Godot.FileAccess.Open(SaveFilePath, Godot.FileAccess.ModeFlags.Read);
+		if (file == null)
+		{
+			GD.PrintErr("GameManager: Failed to open save file for reading.");
+			return;
+		}
+
+		var json = file.GetAsText();
+		file.Close();
+
+		try
+		{
+			SaveData saveData = JsonSerializer.Deserialize<SaveData>(json);
+			if (saveData != null)
+			{
+				CurrentCheckPointID = saveData.CheckPointID;
+				CurrentMaxPoints = saveData.MaxPoints;
+				GD.Print($"GameManager: Loaded checkpoint ID {CurrentCheckPointID} from save file.");
+			}
+			else
+			{
+				GD.PrintErr("GameManager: Save data is null after deserialization.");
+			}
+		}
+		catch (Exception e)
+		{
+			GD.PrintErr($"GameManager: Failed to deserialize save data. Exception: {e.Message}");
+		}
+	}
+
+	public async Task ReturnToLastCheckpoint()
+	{
+		RespawnPlayer(CurrentCheckPointID);
+	}
+
+	public override async void _Input(InputEvent @event)
+	{
+		if (@event is InputEventKey keyEvent)
+		{
+			if (keyEvent.Pressed && keyEvent.Keycode == Key.Escape)
+			{
+				GetTree().ChangeSceneToFile("res://StartMenu/StartMenu.tscn");
+			}
+		}
+	}
+
 }
