7714b753d470b7ce05f24f2994e9b8752ebda8ee
Reyar <2290197845@qq.com>
2026-02-12 00:11:25 +0800

    added quit button

--- StartMenu/QuitButton.cs
+++ StartMenu/QuitButton.cs
@@ -3,4 +3,11 @@ using System;
 
 public partial class QuitButton : Button
 {
+    public override void _Ready()
+    {
+        Pressed += () =>
+        {
+            GetTree().Quit();
+        };
+    }
 }
